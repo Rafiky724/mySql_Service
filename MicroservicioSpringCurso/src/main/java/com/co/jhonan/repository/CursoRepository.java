7136c815e5681bebd768dd91f219f99628142d3c
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-05-18 13:28:06 -0500

    Revert "Parte 3 Parcial Terminado"
    This reverts commit c63372105481534e35d98ef046b6903667b99556.

--- MicroservicioSpringCurso/src/main/java/com/co/jhonan/repository/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.co.jhonan.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.co.jhonan.curso.models.entity.Curso;
-
-public interface CursoRepository extends CrudRepository<Curso, Long> {
-
-}
